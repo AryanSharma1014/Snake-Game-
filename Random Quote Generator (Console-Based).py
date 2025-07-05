0237d583cb13a04a7be21665806cf12805f82486
AryanSharma1014 <aryansharma2004march@gmail.com>
2025-07-05 10:54:21 +0530

    Add files via upload

---
+++ Random Quote Generator (Console-Based).py
@@ -0,0 +1,12 @@
+import random
+
+quotes = [
+    "Believe in yourself!",
+    "Stay positive and happy.",
+    "Never give up!",
+    "Work hard and be kind.",
+    "Dream big."
+]
+
+print("Here's your random quote:")
+print(random.choice(quotes))
